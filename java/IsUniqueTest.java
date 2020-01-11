fc9a9876a7fe33178622b6cc05e7002f0a59df28
Cam Moore <cammymoore07@gmail.com>
2020-01-11 08:55:08 -1000

    Have Java example.
    Need to learn how to use IntelliJ for Java projects.

---
+++ java/IsUniqueTest.java
@@ -0,0 +1,22 @@
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Represents a IsUniqueTest.
+ * @author Cam Moore
+ *
+ */
+class IsUniqueTest {
+
+  /**
+   * Test method for {@link IsUnique#isUnique(java.lang.String)}.
+   */
+  @Test
+  void testIsUnique() {
+    assertTrue("should return true for a unique string", IsUnique.isUnique("abcde"));
+    assertFalse("should return false for a non-unique string", IsUnique.isUnique("abcdee"));
+  }
+
+}
