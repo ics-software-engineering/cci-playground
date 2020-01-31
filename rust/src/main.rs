36d9646dcc63a5fd77dee5504c9556fb81d23fc4
anthonyjchriste <achriste@hawaii.edu>
2020-01-31 10:19:07 -1000

    Update rust CCI examples and tests. Perform some minimal restructuring to be more idiomatic.

---
+++ rust/src/main.rs
@@ -0,0 +1,28 @@
+pub fn main() {
+    let mut my_string = "Rust and CCI are fun! 😉";
+
+    println!(
+        "is_unique('{}') returns {}",
+        my_string,
+        unique::is_unique(my_string)
+    );
+    println!(
+        "is_unique_no_ds('{}') returns {}",
+        my_string,
+        unique::is_unique_no_ds(my_string)
+    );
+
+    my_string = "This workz!😉";
+
+    println!(
+        "is_unique('{}') returns {}",
+        my_string,
+        unique::is_unique(my_string)
+    );
+
+    println!(
+        "is_unique_no_ds('{}') returns {}",
+        my_string,
+        unique::is_unique_no_ds(my_string)
+    );
+}
